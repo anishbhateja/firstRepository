c29153d4a013a50a2b743542272eccae95986475
Anish Bhateja <anishbhateja99@gmail.com>
2020-07-27 15:04:09 +0530

    first commit

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-const express=require('express');
-const app=express();
-const port=8000;
-
+const express = require("express");
+const app = express();
+const port = 8000;
 
 // use express router
-app.use('/', require('./routes'));
+app.use("/", require("./routes"));
 
 //view engine
-app.set('view engine','ejs');
-app.set('views' ,'./views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-
-
-app.listen(port,function(err){
-    if(err){
-        console.log(`Error in running the server: ${port}`); //backtick- includes variables within the string
-    } 
-    else{
-        console.log(`Express server is running on port: ${port}`);
-    }
-})
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running the server: ${port}`); //backtick- includes variables within the string
+  } else {
+    console.log(`Express server is running on port: ${port}`);
+  }
+});
